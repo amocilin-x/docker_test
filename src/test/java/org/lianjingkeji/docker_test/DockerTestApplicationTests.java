810551925340ad2b0a2153769b14cd9ab5d9dc59
hendry <xuesong@lianjingkeji.com>
2024-10-30 17:14:29 +0800

    create

---
+++ src/test/java/org/lianjingkeji/docker_test/DockerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lianjingkeji.docker_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
