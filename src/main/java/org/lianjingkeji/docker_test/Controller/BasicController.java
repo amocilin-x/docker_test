38251f0c4b468ff5a1de90221cbb163b90275542
hendry <xuesong@lianjingkeji.com>
2024-11-05 14:30:33 +0800

    add Dockerfile

--- src/main/java/org/lianjingkeji/docker_test/Controller/BasicController.java
+++ src/main/java/org/lianjingkeji/docker_test/Controller/BasicController.java
@@ -40,7 +40,7 @@ public class BasicController {
     @ResponseBody
     public User user() {
         User user = new User();
-        user.setName("theonefx");
+        user.setName("theonefxdbcsbkaudvboaubdvoqabdjlca");
         user.setAge(666);
         return user;
     }
